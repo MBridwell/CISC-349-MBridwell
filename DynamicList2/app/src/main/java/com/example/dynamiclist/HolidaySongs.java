e923d4944068ec2f913ca9a5d46f052cf6f982ef
MBridwell <masonbridwellcollege@gmail.com>
2025-03-06 14:00:15 -0500

    yea

--- DynamicList2/app/src/main/java/com/example/dynamiclist/HolidaySongs.java
+++ DynamicList2/app/src/main/java/com/example/dynamiclist/HolidaySongs.java
@@ -17,27 +17,49 @@ public class HolidaySongs {
         this.playlist_img = playlist_img;
     }
 
-    public String getAlbumImage() {
+    public String getalbum_image() {
         return album_image;
     }
 
-    public String getAlbumName() {
-        return album_name;
+    public void setalbum_image(String album_image){
+        this.album_image=album_image;
     }
 
-    public String getArtistName() {
+    public String getalbum_name() {
+        return album_name;
+    }
+    public void setalbum_name(String album_name){
+        this.album_name = album_name;
+    }
+
+    public String getartist_name() {
         return artist_name;
     }
 
-    public double getDanceability() {
+    public void setartist_name(String artist_name){
+        this.artist_name=artist_name;
+    }
+
+    public double getdanceability() {
         return danceability;
     }
 
-    public Integer getDurationMs() {
+    public void setdanceability(double danceability){
+        this.danceability = danceability;
+    }
+    public Integer getduration_ms() {
         return duration_ms;
     }
+    public void setduration_ms(Integer duration_ms){
+        this.duration_ms = duration_ms;
+    }
 
-    public String getPlaylistImg() {
+    public String getplaylist_img() {
         return playlist_img;
     }
+
+    public void setplaylist_img(String playlist_img){
+        this.playlist_img = playlist_img;
+    }
+
 }
