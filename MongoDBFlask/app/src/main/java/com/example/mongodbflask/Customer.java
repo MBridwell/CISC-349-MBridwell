101b09ed4aa96a38984b8939c774421e37488655
MBridwell <masonbridwellcollege@gmail.com>
2025-03-18 14:59:40 -0400

    cisc349push

---
+++ MongoDBFlask/app/src/main/java/com/example/mongodbflask/Customer.java
@@ -0,0 +1,11 @@
+package com.example.mongodbflask;
+
+import java.util.List;
+
+public class Customer {
+    protected String _id;
+    protected String customerName;
+    protected String customerPhone;
+    protected String customerAddress;
+    protected List<String> commentList;
+}
