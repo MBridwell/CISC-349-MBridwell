0c68a42410cebc2b4c994030ab0768954ff5de78
MBridwell <masonbridwellcollege@gmail.com>
2025-04-01 14:12:37 -0400

    final commit

---
+++ FinalProject/app/src/main/java/com/example/finalproject/WelcomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.finalproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
